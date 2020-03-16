a26671d110c0e9c32b2e477b14ab3451881aa8e8
Soyeon Jung <soyeonjung@Soyeons-MacBook-Pro.local>
2020-03-16 02:36:33 -0700

    Several changes

--- test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using Revise
-using Test
-using AutomotiveInteraction
-using AutomotiveDrivingModels
-using AutoViz
-
-roadway = make_roadway_interaction()
-render([roadway],camera=StaticCamera(position = VecE2(1000.,1000.)))
-
