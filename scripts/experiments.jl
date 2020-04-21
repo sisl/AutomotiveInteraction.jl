e1eeaae952c138321b7e5876f4e2953f209bdfed
Raunak laptop <raunakbh@stanford.edu>
2020-04-21 12:51:47 +0530

    update gitignore, functions within experiments

--- scripts/experiments.jl
+++ scripts/experiments.jl
@@ -2,36 +2,47 @@
 # Run by changing path in REPL to scripts folder
 # Then `include("experiments.jl")`
 
-
 using AutomotiveSimulator
 using AutomotiveInteraction
 
+"""
+function run_experiment(f;ts=1,id_list=[],dur=10.,modelmaker=make_IDM_models)
+
+- Perform driving simulation and extract metrics
+
+# Arguments
+- ts: start frame
+- id_list: list of vehicles 
+- dur: duration
+- modelmaker: function that makes the models
+
+# Example
+```julia
 f = FilteringEnvironment()
+scenes,collisions = run_experiment(f,id_list=[4,6,8,13,19,28,29],
+modelmaker=make_cidm_models,filename="media/cidm_exp.mp4")
+```
+"""
+function run_experiment(f;ts=1,id_list=[],dur=10.,
+modelmaker=make_IDM_models,filename="media/run_exp.mp4")
+    print("Run experiment from scripts being called \n")
+    scene_real = deepcopy(f.traj[ts])
+    if !isempty(id_list) keep_vehicle_subset!(scene_real,id_list) end
+    
+    models = modelmaker(scene_real)
+    
+    nticks = Int(ceil(duration/f.timestep))
+    scene_list = simulate(scene_real,f.roadway,models,nticks,f.timestep)
+    
+    c_array = test_collision(scene_list,id_list)
+    
+    truth_list = f.traj[start_frame:start_frame+nticks]
+    video_overlay_scenelists(scene_list,truth_list,id_list=id_list,roadway=f.roadway,
+    filename=filename)
 
- # Obtain this from sample_simultanous_vehicles
+    return scene_list, c_array
+end
 
-# Run vehicles using IDM and get a list of scenes
-
-# Perform filtering to obtain driver models
- 
-# Run vehicles using obtained driver models
-
-# Get rmse metrics to compare, and get collision metrics
-
-#********Make idm models and see collision metrics
-start_frame = 1
-id_list = [4,6,8,13,19,28,29]
-duration = 10.
-scene_real = f.traj[start_frame]
-if !isempty(id_list) keep_vehicle_subset!(scene_real,id_list) end
-
-models = make_IDM_models(scene_real)
-
-nticks = Int(ceil(duration/f.timestep))
-scene_list = simulate(scene_real,f.roadway,models,nticks,f.timestep)
-
-c_array = test_collision(scene_list,id_list)
-
-truth_list = f.traj[start_frame:start_frame+nticks]
-video_overlay_scenelists(scene_list,truth_list,id_list=id_list,roadway=f.roadway,
-filename="media/idm_exp.mp4")
+f = FilteringEnvironment()
+scenes,collisions = run_experiment(f,id_list=[4,6,8,13,19,28,29],
+modelmaker=make_cidm_models,filename="media/cidm_exp.mp4")
